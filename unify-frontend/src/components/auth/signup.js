281aec284a856b385db54ae79dc4e74408e13946
Richard Lewis <richard@rlewis.me>
2021-07-13 20:50:42 +0100

    install tailwind

--- unify-frontend/src/components/auth/signup.js
+++ unify-frontend/src/components/auth/signup.js
@@ -20,9 +20,9 @@ const Signup = (props) => {
       if(user.a_password !== '') {return null} // honeypot -> if text in field do not send to server. 
       const res = await signup(user)
 
-      res.isAuthenticated
+      res?.isAuthenticated
       ? history.push(forwardLocation) //change to confirmation page
-      : setSignupError(res.error)
+      : setSignupError(res?.error)
     }
     catch(e){
       setSignupError(e)
@@ -78,13 +78,13 @@ const Signup = (props) => {
 
   const formik = useFormik({
     initialValues: {
-      firstName: urlParams.firstName || '', 
-      lastName: urlParams.lastName || '', 
-      email: urlParams.email || '', 
+      firstName: urlParams?.firstName || '', 
+      lastName: urlParams?.lastName || '', 
+      email: urlParams?.email || '', 
       password: '',
       conPassword: '',
       terms: null,
-      accountId: urlParams.accountId || '1',
+      accountId: urlParams?.accountId || '1',
       a_password: ''
     },
     validationSchema: validationSchema,
