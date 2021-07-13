3b43b096bda8b592285d9e9e7c0b6e9892a4f3d0
Richard Lewis <richard@rlewis.me>
2021-07-13 22:06:10 +0100

    update ui layout

--- unify-frontend/src/App.js
+++ unify-frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 
 import ProtectedRoute from './components/auth/protectedRoute'
 import Nav from './components/home/navSide'
+import Header from './components/home/navTop'
 import Home from './components/home/home'
 import Login from './components/auth/login'
 import Signup from './components/auth/signup'
@@ -25,21 +26,28 @@ const App = () => {
   }, [])
 
   return (
+    
     <div className="md:flex flex-col md:flex-row md:min-h-screen w-full" >
         <BrowserRouter>
           <Nav/>
-          <main>
-              <Switch>
-                  <Route exact path='/' component={Home}/>
-                  <ProtectedRoute exact path='/profile' component={Profile}/>
-                  <ProtectedRoute exact path='/trips' component={TripPicker}/>
-                  <ProtectedRoute exact path='/alerts' component={Alerts}/>
-                  <ProtectedRoute exact path='/preferences' component={Preferences}/>
-                  <Route exact path="/login" component={Login}/>
-                  <Route exact path="/logout" component={Login}/>
-                  <Route exact path="/signup" component={Signup}/>
-              </Switch>
-          </main>
+          <div class="min-h-screen flex flex-col">
+            <Header class="bg-gray-200 p-6 h-24" />
+            <div class="bg-gray-100 p-6 flex-grow">
+              <main>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <ProtectedRoute exact path='/profile' component={Profile}/>
+                    <ProtectedRoute exact path='/trips' component={TripPicker}/>
+                    <ProtectedRoute exact path='/alerts' component={Alerts}/>
+                    <ProtectedRoute exact path='/preferences' component={Preferences}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/logout" component={Login}/>
+                    <Route exact path="/signup" component={Signup}/>
+                </Switch>
+              </main>
+            </div>
+            <div class="bg-gray-200 p-6 h-32">Footer</div>
+          </div>
         </BrowserRouter>
     </div>
   )
