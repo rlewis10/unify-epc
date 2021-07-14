3d2299b6db4d636b53ddf1d3453b50230ff45fde
Richard Lewis <richard@rlewis.me>
2021-07-14 03:28:00 +0100

    update padding

--- unify-frontend/src/App.js
+++ unify-frontend/src/App.js
@@ -32,7 +32,7 @@ const App = () => {
           <Nav/>
           <div className="w-screen min-h-screen flex flex-col">
             <Header className="bg-gray-200 p-8 h-24" />
-            <main className="bg-gray-100 p-8 flex-grow">
+            <main className="bg-gray-100 p-8 flex-grow rounded-t-lg">
               <Switch>
                   <Route exact path='/' component={Home}/>
                   <ProtectedRoute exact path='/profile' component={Profile}/>
@@ -44,7 +44,7 @@ const App = () => {
                   <Route exact path="/signup" component={Signup}/>
               </Switch>
             </main>
-            <div className="bg-gray-200 p-8 h-32">Footer</div>
+            <div className="bg-gray-200 p-8 h-32 rounded-b-lg">Footer</div>
           </div>
         </BrowserRouter>
     </div>
--- unify-frontend/src/components/trips/trip.js
+++ unify-frontend/src/components/trips/trip.js
@@ -5,13 +5,25 @@ const Trip = (props) => {
     const {id, destName, deleteTrip} = props
 
     return (
-        <div className="place">
-            <li>{destName}</li>
-            <button>cruise</button>
-            <button>beach</button>
-            <button>city</button>
-            <button>outdoors</button>
-            <button onClick={() => deleteTrip(id)} className="trash-btn"><i className="fas fa-trash">x</i></button>
+        <div className="max-w-sm rounded-lg overflow-hidden shadow-lg">
+            <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{destName}</div>
+                <p className="text-gray-700 text-base">
+                    test
+                </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <button 
+                    onClick={() => deleteTrip(id)} 
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    </svg>
+                </button>
+            </div>
         </div>
     )
 }
