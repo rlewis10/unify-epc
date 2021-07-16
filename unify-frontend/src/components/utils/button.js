76e1289e90aac6c5fcd870cbf09243e0244141a5
Richard Lewis <richard@rlewis.me>
2021-07-16 16:01:12 +0100

    update tailwind config

--- unify-frontend/src/components/utils/button.js
+++ unify-frontend/src/components/utils/button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
     return (
-        <button {...props} className="group cursor-pointer block items-center px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg hover:text-gray-900 hover:bg-gray-400 focus:text-white focus:bg-blue-500 focus:outline-none focus:shadow-outline">
+        <button {...props} className="group cursor-pointer block items-center px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg hover:text-gray-900 hover:bg-gray-400 focus:text-white focus:bg-blue-500 focus:outline-none focus:shadow-outline ">
             {props.children}
         </button>
     )
--- unify-frontend/src/components/utils/buttonCheckbox.js
+++ unify-frontend/src/components/utils/buttonCheckbox.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const FormCheckbox = (props) => {
 
-    const {id, name,label} = props
+    const {id, name,label, children} = props
     const [isActive, setIsActive] = useState(false)
 
     const toggleClass = () => {
@@ -11,7 +11,12 @@ const FormCheckbox = (props) => {
     }
 
     return (
-        <div className="checkbox-button">
+        <div className={`checkbox-button group pointer-events-auto block items-center px-4 py-2 mt-2 text-sm rounded-lg 
+            ${isActive
+                ? `text-white bg-blue-500 outline-none shadow-outline`
+                : `font-semibold text-gray-900 bg-gray-200 hover:text-gray-900 hover:bg-gray-400 `
+
+            }`}>
             <input 
                 type="checkbox"
                 value={isActive}
@@ -19,10 +24,16 @@ const FormCheckbox = (props) => {
                 defaultChecked={isActive}
                 onChange={toggleClass}
                 onClick={toggleClass}
-                className="hidden"
+                className="group-focus hidden"
             />
             <label htmlFor={id}>
-                {props.children}
+                {
+                    React.Children.map(children, (child) =>
+                    React.cloneElement(child, {
+                        className: `${child.props.className} group-focus`
+                        })
+                    )
+                }
             </label>
         </div>
     )
